bd2d273d5c473ea55343e7ab0ad5a9bf123cc2b1
0211AB <abhay@onelab.com>
2024-06-18 07:53:48 +0530

    updating js imports for pdfmake

--- src/utils/brochure.js
+++ src/utils/brochure.js
@@ -1,5 +1,5 @@
-import pdfMake from "pdfmake"
-import pdfFonts from "pdfmake/build/vfs_fonts"
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
 import { palaBase64 } from '../fonts/pala';
 
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
